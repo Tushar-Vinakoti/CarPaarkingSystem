eeefe50426b0cc68816d3d4df702e237aaafa5e0
Tushar-Vinakoti <tvinakoti@gmail.com>
2025-03-09 17:15:47 +0530

    Update settings.py

--- car_parking_lot/settings.py
+++ car_parking_lot/settings.py
@@ -89,10 +89,9 @@ WSGI_APPLICATION = 'car_parking_lot.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 
